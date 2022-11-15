5261bdab11b0c718deab34c0c04e3fa8f56a9371
michaelamponsah <amponsahmichaelkwame@gmail.com>
2022-11-15 15:34:06 +0000

    Add js script to close nav slider upon clicking  on a link

--- js/script.js
+++ js/script.js
@@ -3,12 +3,18 @@ const body = document.querySelector('body');
 
 document.addEventListener('click', (e) => {
   const isNavIcon = e.target.matches('[data-nav-menu-icon]');
-  if (!isNavIcon) return;
+  const isNavLink = e.target.matches('[data-nav-link]');
 
   if (isNavIcon) {
     navMenu.classList.toggle('mobile-nav-active');
   }
 
+  if(isNavLink && navMenu.classList.contains('mobile-nav-active')) {
+    console.log('hi')
+    navMenu.classList.remove('mobile-nav-active')
+    body.style.overflow = 'auto'
+  }
+  
   if (navMenu.classList.contains('mobile-nav-active')) {
     body.style.overflow = 'hidden';
   } else {
