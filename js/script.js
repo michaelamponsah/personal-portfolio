27412400e8a31856b1715353c04d327c411cbf6b
michaelamponsah <amponsahmichaelkwame@gmail.com>
2022-11-15 14:38:30 +0000

    Fix js linter errors

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-let navMenu = document.querySelector(".mobile-navigation");
-let body = document.querySelector("body");
+const navMenu = document.querySelector('.mobile-navigation');
+const body = document.querySelector('body');
 
-document.addEventListener("click", (e) => {
-  let isNavIcon = e.target.matches("[data-nav-menu-icon]");
+document.addEventListener('click', (e) => {
+  const isNavIcon = e.target.matches('[data-nav-menu-icon]');
   if (!isNavIcon) return;
 
   if (isNavIcon) {
-    navMenu.classList.toggle("mobile-nav-active");
+    navMenu.classList.toggle('mobile-nav-active');
   }
 
-  if (navMenu.classList.contains("mobile-nav-active")) {
-    body.style.overflow = "hidden";
+  if (navMenu.classList.contains('mobile-nav-active')) {
+    body.style.overflow = 'hidden';
   } else {
-    body.style.overflow = "auto";
+    body.style.overflow = 'auto';
   }
-})
+});
