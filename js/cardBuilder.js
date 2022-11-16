da47a5807a1abea762b6811d4e307a07023e60ef
michaelamponsah <amponsahmichaelkwame@gmail.com>
2022-11-16 04:24:58 +0000

    Add cardBuilder.js file
    Created a function to build a works card on the fly

---
+++ js/cardBuilder.js
@@ -0,0 +1,30 @@
+export default cardBuilder = (cardData) => {
+  const card = `
+ <div class="card" tabindex="0">
+ <div class="card-header">
+   <img src="${cardData[imageSource]}"
+     alt="Snapshoot of the homepage of the multi post stories project">
+ </div>
+ <div class="card-details">
+   <div class="card-body">
+     <h3>${cardData[title]}</h3>
+     <ul>
+       <li>Canopy</li>
+       <li><span class="dot"></span> Back End Dev</li>
+       <li><span class="dot"></span>2015</li>
+     </ul>
+     <p>${cardData[description]}</p>
+   </div>
+   <div class="card-footer">
+     <ul>
+       <li>html</li>
+       <li>css</li>
+       <li>javascript</li>
+     </ul>
+     <button class="btn btn-outline">See Project</button>
+   </div>
+ </div>
+</div>
+ `
+ return card;
+}
