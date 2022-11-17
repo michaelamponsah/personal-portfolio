619478d85686b688712051c2626f0449e4a2ed94
michaelamponsah <amponsahmichaelkwame@gmail.com>
2022-11-17 12:20:35 +0000

    Remove comments and unused code

--- js/utils.js
+++ js/utils.js
@@ -13,15 +13,15 @@ export const getCardData = (cardData) => {
 };
 
 export const setModalDisplayData = (displayData) => {
-
-  const { image, title, description, sources } = displayData;
+  const { 
+    image, title, description, sources 
+  } = displayData;
   document.querySelector('.modal--title').innerText = title;
   document.querySelector('.modal--image').src = image.src;
   document.querySelector('.modal--image').alt = image.alt;
   document.querySelector('.modal--description').innerText = description;
   document.querySelector('.modal--see-live').href = sources.seeLive;
   document.querySelector('.modal--see-source').href = sources.seeSource;
-
 };
 
 export const disableScrolling = () => {
